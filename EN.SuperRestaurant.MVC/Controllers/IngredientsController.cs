3204d35ac6e9f1d4124f555709cad65f20417783
MibAX <154632432+MibAX@users.noreply.github.com>
2024-08-06 22:58:00 +0300

    - Added Ingredients Controller - Added Ingredients Index Link to _Layout

---
+++ EN.SuperRestaurant.MVC/Controllers/IngredientsController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EN.SuperRestaurant.Entities.Ingredients;
+using EN.SuperRestaurant.MVC.Data;
+
+namespace EN.SuperRestaurant.MVC.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        #region Data & Const
+
+        private readonly ApplicationDbContext _context;
+
+        public IngredientsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Actions
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ingredients.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            return View(ingredient);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ingredient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingredient);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ingredient ingredient)
+        {
+            if (id != ingredient.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ingredient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IngredientExists(ingredient.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingredient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ingredient = await _context.Ingredients.FindAsync(id);
+            if (ingredient != null)
+            {
+                _context.Ingredients.Remove(ingredient);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private bool IngredientExists(int id)
+        {
+            return _context.Ingredients.Any(e => e.Id == id);
+        } 
+
+        #endregion
+    }
+}
